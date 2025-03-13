0269d4d5c4a8523a99fca328d5ca135037557336
KaiioBr <kaiomae8@gmail.com>
2025-03-13 16:17:17 +0000

    save

--- 97744-logica-main/97744-logica-main/0. a_testes.py/8. teste.py
+++
@@ -1,9 +0,0 @@
-import os
-
-os.system("clear") # Limpa o Terminal.
-
-print('\t====tabuada====')
-numero = float(input("Digite um numero: "))
-print('\n Tabuada de', numero, ':')
-for i in range(-9, 11):
-    print(numero, 'X', i, '=', (numero * i))
--- 97744-logica-main/97744-logica-main/1. conceitos inicias/1. ola_mundo.py
+++
@@ -1,3 +0,0 @@
-print("Olá, mundo")
-print("Kaiio Brandão")
-
